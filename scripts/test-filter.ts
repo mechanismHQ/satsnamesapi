07c03e947a9bc8e988c7524f9906b932c588fee6
Ryan Waits <ryan.waits@gmail.com>
2023-07-03 06:48:15 -0500

    log filtered name

--- scripts/test-filter.ts
+++ scripts/test-filter.ts
@@ -1,9 +1,4 @@
-import {
-  fetchOrdinals,
-  filterInscriptions,
-  fetchAndFilterInscriptions,
-} from "../src/ordinals-api";
-// import { fetchAndFilterInscriptions } from "../src/ord-api";
+import { fetchAndFilterInscriptions } from "../src/ordinals-api";
 
 const [indexStr] = process.argv.slice(2);
 
@@ -12,7 +7,11 @@ async function run() {
   const filtered = await fetchAndFilterInscriptions(index - 1);
   // const list = await fetchOrdinals(index - 1);
   // const filtered = await filterInscriptions(list);
-  console.log(filtered.inscriptions.map((i) => i._name));
+  const name = filtered.inscriptions.slice(0, 1).map((i) => i._name)[0];
+  if (name) {
+    console.log({ name, encoded: encodeURIComponent(name) });
+    console.log(encodeURIComponent(name));
+  }
   console.log("max ID:", filtered.maxId);
 }
 
